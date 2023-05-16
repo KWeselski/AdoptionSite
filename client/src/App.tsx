2f9b478a611a5926a96a8f4c3bccb71dc78b2101
KWeselski <weselski.kamil@gmail.com>
2023-05-16 11:47:33 +0200

    Frontend update, first pages

---
+++ client/src/App.tsx
@@ -0,0 +1,39 @@
+import "./App.css";
+import NavBar from "./components/NavBar";
+import Home from "./pages/Home";
+import { Route, Routes } from "react-router-dom";
+import styles from "./styles";
+import Footer from "./components/Footer";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import AdoptionPage from "./pages/AdoptionPage";
+import AnimalsList from "./pages/AnimalsList";
+
+function App() {
+  return (
+    <div className="bg-[#F7FFE6] w-full overflow-hidden">
+      <div className={`${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>
+          <NavBar />
+        </div>
+      </div>
+      <div className="min-h-screen">
+        <div className={`${styles.flexStart}`}>
+          <div className={`${styles.boxWidth}`}>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/register" element={<Register />} />
+              <Route path="/animals/:id" element={<AdoptionPage />} />
+              <Route path="/animals/" element={<AnimalsList />} />
+            </Routes>
+          </div>
+        </div>
+      </div>
+
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
